1a8f9cef9b4fba21a7bc05e6bac5b863864eeb94
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-04-10 19:06:13 +0100

    mobile view for steps page

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -6,18 +6,18 @@ const Steps = () => {
         <div className="w-[100vw] pb-32 relative rounded-tl-[7.5%] bg-white rounded-tr-[7.5%] px-[8vw] pt-12">
             <div className="flex justify-between">
                 <div className='border-2 mb-16 border-white pt-16 border-b-slate-500 pb-6 '>
-                    <p className='text-slate-800 text-6xl  font-bold'>HOW IT WORKS <span className='text-2xl text-slate-500'>/</span><span className='text-xl font-bold text-slate-500'> East Steps</span></p>
+                    <p className='text-slate-800 lg:text-6xl md:text-5xl text-6xl  font-bold'>HOW IT WORKS <span className='text-2xl text-slate-500'>/</span><span className='text-xl font-bold text-slate-500'> East Steps</span></p>
 
                 </div>
             </div>
-            <div className='flex flex-row md:flex-wrap justify-evenly mt-8'>
-                <div className="flex border bg-slate-300 rounded-2xl w-[300px]  h-[350px] p-4 flex-col justify-between items-start px-4">
+            <div className='flex lg:flex-row flex-col md:flex-wrap justify-evenly mt-8'>
+                <div className="flex border bg-slate-300 rounded-2xl w-[300px] lg:mb-0 md:mb-0 mb-4 h-[350px] p-4 flex-col justify-between items-start px-4">
                     <div className='border p-5 rounded-full bg-white'>
                         <AirportShuttleIcon style={{ fontSize: '32px' }} />
                     </div>
                     <p className='text-2xl font-bold text-white'>Track <br /> Selection</p>
                 </div>
-                <div className="flex border bg-slate-300 rounded-2xl w-[300px]  h-[350px] p-4 flex-col justify-between items-start px-4">
+                <div className="flex border bg-slate-300 rounded-2xl w-[300px] lg:mb-0 md:mb-0 mb-4  h-[350px] p-4 flex-col justify-between items-start px-4">
                     <div className='border p-5 rounded-full bg-white'>
                         <InventoryIcon style={{ fontSize: '32px' }} />
                     </div>
